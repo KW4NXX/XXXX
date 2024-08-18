29d8f28cc8d665d06ef1aff4c78a4cc344d6d6a7
KW4NXX <peepoo182@gmail.com>
2024-08-18 22:17:54 +0700

    first commit

---
+++ src/app/Home/page.js
@@ -0,0 +1,12 @@
+import react from "react";
+function Home (){
+    return( 
+        <center>
+<div>
+    <h1>Home</h1>
+    <p>This Home Page </p>
+</div>   
+</center>
+    )
+}
+export default Home
